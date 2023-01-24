bee961e1e22bad8f85d26e57c768b04133fdc512
추준성 <55497825+031wnstjd@users.noreply.github.com>
2023-01-25 00:06:37 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -6,36 +6,21 @@
 using namespace std;
 
 int arr[N];
-vector<int> v;
-
-void comb(int start, vector<int> v) {
-	if(v.size() == K) {
-		int sum = 0;
-		for (int val : v) {
-			sum += val;
-		}
-		
-		if(sum == 100) {
-			for (int val : v) cout << val << '\n';
-			exit(0);
-		}
-		
-		return;
-	}
-
-	for (int i = start + 1; i < N; i++) {
-		v.push_back(arr[i]);
-		comb(i, v);
-		v.pop_back();
-	}
-}
 
 int main() {
 	for (int i = 0; i < N; i++) {
 		cin >> arr[i];
 	}
 	sort(arr, arr+N);
-	comb(-1, v);
+	
+	do {
+		int sum = 0;
+		for (int i = 0; i < K; i++) sum += arr[i];
+		if(sum == 100) break;
+		
+	} while(next_permutation(arr, arr + 9));
+	
+	for (int i = 0; i < K; i++) cout << arr[i] << '\n';	
 	
 	return 0;
 }

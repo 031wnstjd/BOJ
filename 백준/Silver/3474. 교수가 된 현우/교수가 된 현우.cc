a44c41ac6e088d3cd4bb9e48ff46189c75dad603
추준성 <55497825+031wnstjd@users.noreply.github.com>
2023-02-05 18:24:52 +0900

    [Silver III] Title: 교수가 된 현우, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+/*
+	오른쪽 끝의 0의 개수 ==  10을 곱한 개수 == 2*5의 개수 == min(2의 개수, 5의 개수) 
+*/
+using namespace std;
+
+typedef long long ll;
+
+int TC, N;
+ll cnt_two, cnt_five;
+
+int main() {
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin >> TC;
+	for (int tc = 0; tc < TC; tc++) {
+		cin >> N;
+		cnt_two = 0; cnt_five = 0;
+		for(int i = 2; i <= N; i *= 2) cnt_two += (N / i);
+		for (int j = 5; j <= N; j *= 5) cnt_five += (N / j);
+		cout << min(cnt_two, cnt_five) << '\n';
+	}
+		
+	return 0;
+}

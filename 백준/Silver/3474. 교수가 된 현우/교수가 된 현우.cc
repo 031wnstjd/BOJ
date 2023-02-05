0f3879dd9bcbc7f535ba2b77a4d74486a76ba478
추준성 <55497825+031wnstjd@users.noreply.github.com>
2023-02-05 18:28:10 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 /*
-	오른쪽 끝의 0의 개수 ==  10을 곱한 개수 == 2*5의 개수 == min(2의 개수, 5의 개수) 
+	오른쪽 끝의 0의 개수 ==  10을 곱한 개수 == 2*5의 개수 == min(2의 개수, 5의 개수) == 5의 개수 (항상 5로 나눈 경우의 수가 더 작기 때문) 
 */
 using namespace std;
 
@@ -15,9 +15,10 @@ int main() {
 	for (int tc = 0; tc < TC; tc++) {
 		cin >> N;
 		cnt_two = 0; cnt_five = 0;
-		for(int i = 2; i <= N; i *= 2) cnt_two += (N / i);
+//		for(int i = 2; i <= N; i *= 2) cnt_two += (N / i);
 		for (int j = 5; j <= N; j *= 5) cnt_five += (N / j);
-		cout << min(cnt_two, cnt_five) << '\n';
+//		cout << min(cnt_two, cnt_five) << '\n';
+		cout << cnt_five << '\n'; 
 	}
 		
 	return 0;

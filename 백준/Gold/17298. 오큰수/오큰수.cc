03917495949bfb22cca93bfc6e1672b5d6a416a7
추준성 <55497825+031wnstjd@users.noreply.github.com>
2023-03-02 23:52:08 +0900

    [Gold IV] Title: 오큰수, Time: 272 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -4,25 +4,20 @@
 
 using namespace std;
 
-stack<pair<int, int>> stk;
-vector<pair<int, int>> v;
-int N, val;
+stack<int> stk;
+int N, a[MAX_N], ret[MAX_N];
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		cin >> val;
-		while(stk.size() && stk.top().second < val) {
-			v.push_back({stk.top().first, val}); 
+	memset(ret, -1, sizeof(ret));
+	for (int i = 0; i < N; i++) {
+		cin >> a[i];
+		while(stk.size() && a[stk.top()] < a[i]) {
+			ret[stk.top()] = a[i];
 			stk.pop();
 		}
-		stk.push({i, val});
+		stk.push(i);
 	}
-	while(stk.size()) {
-		v.push_back({stk.top().first, -1});
-		stk.pop();
-	}
-	sort(v.begin(), v.end());
-	for (auto ret : v) cout << ret.second << " ";
+	for (int i = 0; i < N; i++) cout << ret[i] << " ";
 }
